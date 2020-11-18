5f8a1731474dabe78bde5fca773e47619ba26c2f
SvetlanaGoncharuk <svetlana.goncharuk19@gmail.com>
2020-11-18 21:48:42 +0200

    first commit

---
+++ 1.js
@@ -0,0 +1,9 @@
+let age = parseInt(prompt('Введите Ваш возраст'));
+
+if (age % 10 === 1) {
+    alert('Вам ' + age + ' год');
+} else if ((age % 10 === 2 && age != 12) || (age % 10 === 3 && age != 13) || (age % 10 === 4 && age != 14)) {
+    alert('Вам ' + age + ' года');
+} else {
+    alert('Вам ' + age + ' лет');
+}
