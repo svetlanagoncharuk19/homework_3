8724b999ff51ce43906cce1631257988e7616e88
svetlanagoncharuk19 <52744113+svetlanagoncharuk19@users.noreply.github.com>
2020-11-21 18:55:23 +0200

    Update 1.js

--- 1.js
+++ 1.js
@@ -2,7 +2,7 @@ let age = parseInt(prompt('Введите Ваш возраст'));
 
 if (age % 10 === 1 && age % 100 !== 11) {
     alert('Вам ' + age + ' год');
-} else if ((age % 10 === 2 && age !== 12) || (age % 10 === 3 && age !== 13) || (age % 10 === 4 && age !== 14)) {
+} else if ((age % 10 === 2 && age % 100 !== 12) || (age % 10 === 3 && age % 100 !== 13) || (age % 10 === 4 && age % 100 !== 14)) {
     alert('Вам ' + age + ' года');
 } else {
     alert('Вам ' + age + ' лет');
